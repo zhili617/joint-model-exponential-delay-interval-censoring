68f5b8ab0dd99dd181ea6c2e20089187ebf700ce
zhili617 <zhilijia@student.ubc.ca>
2026-04-16 16:32:28 -0700

    right-censored + itnerval censored

--- orin/HHJMs-master/R/evalMat.r
+++ orin/HHJMs-master/R/evalMat.r
@@ -1,4 +1,3 @@
-
 # evaluate a matrix 
 
 evalMat <- function(mat, q, data=NULL, par.val=NULL, raneff.val=NULL){
@@ -11,11 +10,16 @@ evalMat <- function(mat, q, data=NULL, par.val=NULL, raneff.val=NULL){
   for(i in 1:q){
     for(j in 1:q){
       kk <- (i-1)*q+j
-      Di <- with(data, with(par.val, with(raneff.val,  
-                eval(parse(text=mat[kk])))))
-
+      Di <- with(data, with(par.val, with(raneff.val,
+                                          eval(parse(text=mat[kk])))))
+      
+      # If the expression is a constant (no data variable), with(data,...) returns
+      # a scalar instead of a length-N vector, so we must multiply by nrow(data).
+      n_obs <- if (!is.null(data)) nrow(data) else 1
+      if (length(Di) == 1 && n_obs > 1) Di <- Di * n_obs
+      
       D[i,j] <- sum(Di)
-     }
+    }
   }
   
   return(D)
